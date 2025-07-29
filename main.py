24584f932e1ca74be52ee4cd7c8796d8866a937b
KYI-m2 <webapplication.m2@gmail.com>
2025-07-29 15:36:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import streamlit as st
 import streamlit_option_menu
+with st.sidebar :
+  tab = option_menu (
+    "Sci-Day",
+    ['Home','About Sci-Day','Activities'],
+    icon = ['home','book','game']
+  )
